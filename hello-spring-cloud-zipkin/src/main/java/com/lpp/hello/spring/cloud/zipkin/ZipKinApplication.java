39522e3f569f343d60d8938c1fd05775e24fd17c
admin <17864308317@163.com>
2019-09-11 15:11:46 +0800

    finished

--- hello-spring-cloud-zipkin/src/main/java/com/lpp/hello/spring/cloud/zipkin/ZipKinApplication.java
+++ hello-spring-cloud-zipkin/src/main/java/com/lpp/hello/spring/cloud/zipkin/ZipKinApplication.java
@@ -3,10 +3,15 @@ package com.lpp.hello.spring.cloud.zipkin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin.server.internal.EnableZipkinServer;
 
 
+/**
+ * @EnableZipkinServer 开启ZipKin Server功能
+ */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableZipkinServer
 public class ZipKinApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZipKinApplication.class,args);
